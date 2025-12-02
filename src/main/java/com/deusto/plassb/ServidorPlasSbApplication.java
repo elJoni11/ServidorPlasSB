9c2db4049190373fc2b6eb7a907bd417ff1838c6
Jon Ander Areizaga <jonander.areizaga@opendeusto.es>
2025-12-02 18:07:10 +0100

    creación del servidor PlasSB

---
+++ src/main/java/com/deusto/plassb/ServidorPlasSbApplication.java
@@ -0,0 +1,13 @@
+package com.deusto.plassb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorPlasSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorPlasSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deusto/plassb/ServidorPlasSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusto.plassb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorPlasSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
